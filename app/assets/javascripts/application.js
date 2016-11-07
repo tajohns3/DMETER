6059e85790cef4ef4a7d99c1efbd5a4b03c672e7
Christopher <martinez10304@gmail.com>
2016-11-06 18:03:19 -0600

    New Upload Image Buttons Complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,133 @@
 //= require underscore
 //= require_tree .
 
+//for upload dealer reg form on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture").change(function(){
+        readURL(this);
+    });
+});
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//for upload dealer photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture1").change(function(){
+        readUR(this);
+    });
+});
+function readUR(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview1').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//for upload fl photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture2").change(function(){
+        readU(this);
+    });
+});
+function readU(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview2').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//for upload cst photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture3").change(function(){
+        readUw(this);
+    });
+});
+function readUw(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview3').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//for upload sst photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture4").change(function(){
+        readUwt(this);
+    });
+});
+function readUwt(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview4').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+//for upload vat photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture5").change(function(){
+        readUwtt(this);
+    });
+});
+function readUwtt(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview5').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
+//for upload pancard photo on dealers form
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture6").change(function(){
+        readUwttt(this);
+    });
+});
+function readUwttt(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview6').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 //for the ajax call to get demonstration to appear in fa acts
 $('.dropdown-toggle').dropdown()
 
